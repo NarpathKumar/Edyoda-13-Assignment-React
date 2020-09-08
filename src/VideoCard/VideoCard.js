f2c8700b8fed94136dfe4b58b3e0ef2ce92d0640
NarpathKumar <53571476+NarpathKumar@users.noreply.github.com>
2020-09-08 16:17:13 +0530

    Update VideoCard.js

--- src/VideoCard/VideoCard.js
+++ src/VideoCard/VideoCard.js
@@ -7,7 +7,10 @@ const VideoCard = (props) => {
        border: "2px solid yellow"
    }
     return(
-        <div onClick={()=>props.history.push(`/${props.id}`)} className={classes.cardDiv} style={props.active===props.id?styleObj:null}  >
+        <div onClick={()=>{
+            window.scrollTo(0,0);
+            props.history.push(`/${props.id}`)
+        }} className={classes.cardDiv} style={props.active===props.id?styleObj:null}  >
             <img className={classes.Thumbnail} src={props.thumbnail} alt="Video Thumbnail" />
             <h3 className={classes.Title}>{props.title}</h3>
         </div>
